96b00723a73906cf12b7ec87d2269a4c4d9da58e
masaki.sato <merume@gmail.com>
2024-05-21 21:47:32 +0900

    test

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   let!(:user) { create(:user, first_name: "Suzuki", last_name: "Jiro") }
   describe "User#full_name" do
     it '"first_name last_name"が返ってくる' do
-      expect(user.full_name).to eq "#{user.first_name} #{user.last_name}+3"
+      expect(user.full_name).to eq "#{user.first_name} #{user.last_name}+1"
     end
   end
 end
